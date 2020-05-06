d0396582adb07524324890c8beb147c9f35c695b
oleg.shilo <oleg.shilo@leicabiosystems.com>
2020-05-06 21:47:06 +1000

    Release v1.11.2
    * Issue #33: Codemap generic mapper does not work with UTF-16 encoded files.
    Changed (from reading doc content from the file) to reading the doc content from the text editor buffer. So no dependency on the file encoding.Fix

--- src/mapper_generic.ts
+++ src/mapper_generic.ts
@@ -5,115 +5,110 @@ import { Uri, commands } from "vscode";
 import { Utils } from './utils';
 
 export interface SyntaxMapping {
-	pattern: string;
-	clear: string;
-	suffix: string;
-	prefix: string;
-	regex: RegExp;
-	role: string;
-	icon: string;
+    pattern: string;
+    clear: string;
+    suffix: string;
+    prefix: string;
+    regex: RegExp;
+    role: string;
+    icon: string;
 }
 
 export class mapper {
 
-	public static generate(file: string, mappings: SyntaxMapping[]): string[] {
+    public static generate(file: string, mappings: SyntaxMapping[]): string[] {
 
-		// # Parse
-		let item_max_length = 0;
-		let members = [];
+        // # Parse
+        let item_max_length = 0;
+        let members = [];
 
-		try {
+        try {
 
-			mappings.forEach(item => item.regex = new RegExp(item.pattern, 'g'));
+            mappings.forEach(item => item.regex = new RegExp(item.pattern, 'g'));
 
-			let lines: string[];
+            let lines: string[];
 
-			if (!fs.existsSync(file)) {
+            let text = vscode.window.activeTextEditor.document.getText();
+            lines = text.split(/\r?\n/g);
 
-				let text = vscode.window.activeTextEditor.document.getText();
-				lines = text.split(/\r?\n/g);
-			}
-			else
-				lines = Utils.read_all_lines(file);
+            let line_num = 0;
+            let last_type = '';
+            let last_indent = 0;
 
-			let line_num = 0;
-			let last_type = '';
-			let last_indent = 0;
+            lines.forEach(line => {
 
-			lines.forEach(line => {
+                line_num = line_num + 1;
+                line = line.replace('\t', '    ');
 
-				line_num = line_num + 1;
-				line = line.replace('\t', '    ');
+                if (line != '') {
+                    let code_line = line.trimStart();
 
-				if (line != '') {
-					let code_line = line.trimStart();
+                    let indent_level = line.length - code_line.length;
 
-					let indent_level = line.length - code_line.length;
+                    for (let item of mappings) {
 
-					for (let item of mappings) {
+                        let m = line.match(item.regex);
 
-						let m = line.match(item.regex);
+                        if (m) {
+                            let match = m[0];
 
-						if (m) {
-							let match = m[0];
+                            if (item.clear)
+                                item.clear
+                                    .split('|')
+                                    .forEach(text => {
+                                        try {
+                                            match = match.replaceAll(text, '');
+                                        }
+                                        catch (error) {
+                                            console.log(error);
+                                        }
+                                    });
 
-							if (item.clear)
-								item.clear
-									.split('|')
-									.forEach(text => {
-										try {
-											match = match.replaceAll(text, '');
-										}
-										catch (error) {
-											console.log(error);
-										}
-									});
+                            if (item.suffix)
+                                match += item.suffix;
+                            if (item.prefix)
+                                match = item.prefix + match;
 
-							if (item.suffix)
-								match += item.suffix;
-							if (item.prefix)
-								match = item.prefix + match;
+                            members.push([line_num, item.role, match, indent_level, item.icon]);
+                            break;
+                        }
+                    }
+                }
+            });
+        } catch (error) {
+            members = [];
+        }
 
-							members.push([line_num, item.role, match, indent_level, item.icon]);
-							break;
-						}
-					}
-				}
-			});
-		} catch (error) {
-			members = [];
-		}
+        // format
+        let map = '';
+        let last_indent = 0;
+        let last_type = '';
 
-		// format
-		let map = '';
-		let last_indent = 0;
-		let last_type = '';
+        members.forEach(item => {
+            try {
+                let line = item[0];
+                let content_type = item[1];
+                let content = item[2];
+                let indent = item[3];
+                let icon = item[4];
+                let extra_line = '';
 
-		members.forEach(item => {
-			try {
-				let line = item[0];
-				let content_type = item[1];
-				let content = item[2];
-				let indent = item[3];
-				let icon = item[4];
-				let extra_line = '';
+                if (indent == last_indent && content_type != last_type)
+                    extra_line = '\n';
 
-				if (indent == last_indent && content_type != last_type)
-					extra_line = '\n';
+                let prefix = ' '.repeat(indent);
+                let lean_content = content.trimStart();
 
-				let prefix = ' '.repeat(indent);
-				let lean_content = content.trimStart();
+                map = map + extra_line + prefix + lean_content + '|' + String(line) + '|' + icon + '\n';
 
-				map = map + extra_line + prefix + lean_content + '|' + String(line) + '|' + icon + '\n';
+                last_indent = indent;
+                last_type = content_type;
 
-				last_indent = indent;
-				last_type = content_type;
+            } catch (error) {
+                console.log('');
+            }
+        });
 
-			} catch (error) {
-				console.log('');
-			}
-		});
-
-		return map.trim().lines();
-	}
+        return map.trim().lines();
+    }
 }
