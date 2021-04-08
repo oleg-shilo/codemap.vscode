e4ebdf5c12d9a84e01f71780368f3517d7176e89
Sush <figurcoe@web.de>
2021-04-08 17:13:37 +0200

    Update mapper_tcl.ts
    - Removed unnecessary "export", which caused issues
    - Slightly cleaned up regexs
    - Replacing tabs with 4 spaces to prevent issues, when files were edited with different editors, leaving a mixture between 4-spaces- and tab-indentation

--- src/mapper_tcl.ts
+++ src/mapper_tcl.ts
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 
-export class mapper {
+class mapper {
 
     static read_all_lines(file) {
         let text = fs.readFileSync(file, 'utf8');
@@ -14,19 +14,19 @@ export class mapper {
         try {
             let line_num = 0; 
 
-            const re_proc = new RegExp('^(\\s*)proc\\s+(.+?)(?:\\s*{\\s*)$');
-            const re_class = new RegExp('^(\\s*)(?:(?:::)?itcl::)?class\\s+([\\w:]+)');
-            const re_variable = new RegExp('^(\\s*)(?:(?:public|private|protected)?\\s+)?variable\\s+(\\w+)');
-            const re_constructor = new RegExp('^(\\s*)constructor\\s+(.+?)(?:\\s*{\\s*)$');
-            const re_destructor = new RegExp('^(\\s*)destructor');
-            const re_method = new RegExp('^(\\s*)(?:(?:public|private|protected)?\\s+)?method\\s+(\\w+.+)(?:\\s*{\\s*)$');
-            const re_namespace= new RegExp('^(\\s*)(?:namespace\\s+eval)\\s+(\\w+)');
+            const re_proc =        /^(\s*)proc\s+(.+?)(?:\s*{\s*)$/;
+            const re_class =       /^(\s*)(?:(?:::)?itcl::)?class\s+([\w:]+)/;
+            const re_variable =    /^(\s*)(?:(?:public|private|protected)?\s+)?variable\s+(\w+)/;
+            const re_constructor = /^(\s*)constructor\s+(.+?)(?:\s*{\s*)$/;
+            const re_destructor =  /^(\s*)destructor/;
+            const re_method =      /^(\s*)(?:(?:public|private|protected)?\s+)?method\s+(\w+.+)(?:\s*{\s*)$/;
+            const re_namespace=    /^(\s*)(?:namespace\s+eval)\s+((?:::)?\w+)/;
 
             mapper
                 .read_all_lines(file)
                 .forEach(line => {
                     line_num++;
-                    //line = line.trimStart();
+                    line = line.replace(/\t/g, '    ');
                     // proc
                     if (re_proc.test(line)) {
                         let result = line.match(re_proc);
